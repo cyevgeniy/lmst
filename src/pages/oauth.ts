0a850048b0bbde702b8081b8012aed38489f4c16
Evgeny Chaban <ychbn@ychbn.com>
2024-02-28 11:31:12 +0300

    chore: remove unused code

--- src/pages/oauth.ts
+++ src/pages/oauth.ts
@@ -33,33 +33,3 @@ export class OAuthPage extends Page implements IPage {
     }  
   }
 }
-
-export const oauthPage = definePage(() => {
-  async function onParamsChange() {
-    // get authorization code from url search string
-    // @ts-ignore
-    const searchParams = new URL(window.location).searchParams
-    const code = searchParams.get('code')
-
-    const user = new User()
-    const res = await user.getUserToken(code ?? '')
-
-    if (!res.ok) {
-      console.error(res.error)
-      window.location.replace('/')
-    }
-    else {
-      user.verifyCredentials()
-      window.location.replace('/')
-    }
-  }
-
-  function mount(): HTMLElement {
-    return div('', []) // 
-  }
-
-  return {
-    mount,
-    onParamsChange,
-  }
-})

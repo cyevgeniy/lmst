0356ab467e858cfe70cbd272a669fb00f8f1ebdb
Evgeny Chaban <ychbn@ychbn.com>
2024-02-28 11:12:39 +0300

    chore: remove unused  code

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { useAppConfig } from "./appConfig"
 import { User } from "./utils/user"
 import { StatusManager, ProfileTimelineManager, TimelineManager } from './appManager'
 
-const buttons = document.createElement('div')
-buttons.innerHTML = `<button id="btn"> Profile </div>
-<button id="btn1"> Settings </div>`
-
-document.getElementById('app')?.appendChild(buttons)
-
 document.getElementById('btn')?.addEventListener('click', () => {
   lRouter.navigateTo('/profile/13/question/question-123/')
 })

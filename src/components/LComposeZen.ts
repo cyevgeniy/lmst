aef3cd7c79d71ab2e719873355c43afe8fe56167
Evgeny Chaban <hi@evg.moe>
2024-07-13 02:26:43 +0300

    wip(compose): add zen mode

--- src/components/LComposeZen.ts
+++ src/components/LComposeZen.ts
@@ -1,13 +1,15 @@
 import { div, h } from '../utils/dom'
 import { LButton } from './LButton'
+import { onClick } from '../utils/events'
 
 export class LComposeZen {
-  private el: HTMLElement
+  public readonly el: HTMLElement
   private textarea: HTMLTextAreaElement
   private btn: LButton
+  private onCloseCb: () => void
 
   constructor(root: HTMLElement) {
-    this.btn = new LButton('Post', 'compose-zen__button')
+    this.btn = new LButton('Post', ['compose-zen__button'])
     this.el = div('compose-zen')
     const wrapper = div('compose-zen-wrapper')
     this.textarea = h('textarea', {
@@ -21,13 +23,24 @@ export class LComposeZen {
     wrapper.appendChild(this.btn.el)
     this.el.appendChild(wrapper)
     root.appendChild(this.el)
+    this.setFocus()
+    this.onCloseCb = () => {}
+    onClick(this.btn.el, () => this.onCloseCb())
   }
 
   public getText() {
     return this.textarea.value
   }
 
+  public setText(t: string) {
+    this.textarea.value = t
+  }
+
   public setFocus() {
     this.textarea.focus()
+  }
+
+  public onClose(cb: () => void) {
+    this.onCloseCb = cb
   }
 }

9fad04f3cf986fcc471c2c27ae808f282539e180
Evgeny Chaban <hi@evg.moe>
2024-08-04 12:39:04 +0300

    refactor(linfo): use function

--- src/components/LInfo.ts
+++ src/components/LInfo.ts
@@ -1,33 +1,35 @@
 import { useAppConfig } from "../appConfig";
 import { a, div, h, span } from "../utils/dom";
 
-export class LInfo {
-  private serverInfo: HTMLElement
-  constructor(root: HTMLElement) {
-    const appConfig = useAppConfig()
+export function LInfo() {
+  const appConfig = useAppConfig()
 
-    this.serverInfo = a('', appConfig.server, appConfig.server)
-    root.appendChild(div('infoblock', [
-      h('h2', null, 'Client Info'),
-      div('', [
-        span('', 'Server: '),
-        this.serverInfo,
-      ]),
+  const serverInfo = a('', appConfig.server, appConfig.server)
 
-      div('', [
-        span('', 'Version: '),
-        span('', appConfig.version),
-      ]),
+  const el = div('infoblock', [
+    h('h2', null, 'Client Info'),
+    div('', [
+      span('', 'Server: '),
+      serverInfo,
+    ]),
 
-      div('', [
-        span('', 'Repository: '),
-        a('', appConfig.repo, appConfig.repo),
-      ])
-    ]))
+    div('', [
+      span('', 'Version: '),
+      span('', appConfig.version),
+    ]),
 
-    appConfig.addOnServerChangeCb((server) => {
-      this.serverInfo.setAttribute('href', server)
-      this.serverInfo.innerText = server
-    })
+    div('', [
+      span('', 'Repository: '),
+      a('', appConfig.repo, appConfig.repo),
+    ])
+  ])
+
+  appConfig.addOnServerChangeCb((server) => {
+    serverInfo.setAttribute('href', server)
+    serverInfo.innerText = server
+  })
+
+  return {
+    el,
   }
 }

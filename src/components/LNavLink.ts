2fa8067498ee053f368a30d48ffc03b1d8b1de52
Evgeny Chaban <hi@evg.moe>
2024-08-21 21:43:51 +0300

    chore: remove unused code

--- src/components/LNavLink.ts
+++ src/components/LNavLink.ts
@@ -1,4 +1,4 @@
-import { h, hide, HTMLEventHandler, show, useCommonEl } from '../utils/dom'
+import { h, HTMLEventHandler, useCommonEl } from '../utils/dom'
 
 export type NavLinkProps = {
   text: string
@@ -27,38 +27,5 @@ export function LNavLink(props: NavLinkProps) {
       if (v) show()
       else hide()
     }
-  }
-}
-
-
-export class lLNavLink {
-  public readonly el: HTMLAnchorElement
-
-  constructor(opts: {
-    text: string
-    link: string
-    icon?: string
-  }) {
-    this.el = h('a', {
-      className: 'navBar-link',
-      innerHTML: `${opts.icon ?? ''}${opts.text}`,
-      attrs: { href: opts.link}
-    })
-  }
-
-  public setLink(link: string) {
-    this.el.href = link
-  }
-
-  public setText(text: string) {
-    this.el.innerText = text
-  }
-
-  public show() {
-    show(this.el)
-  }
-
-  public hide() {
-    hide(this.el)
   }
 }
--- src/stores/useTimeline.ts
+++
@@ -1,9 +0,0 @@
-import type { Status } from '../types/shared.d.ts'
-
-const timeline: Status[] = []
-
-export function useTimeline() {
-  return {
-    timeline,
-  }
-}

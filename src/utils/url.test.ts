2319c782171f6a647704e9daca15658b544990a7
Evgeny Chaban <hi@evg.moe>
2024-09-27 17:51:49 +0000

    test(url): add a few tests

---
+++ src/utils/url.test.ts
@@ -0,0 +1,23 @@
+import t from 'tap'
+import { searchParams } from './url.ts'
+
+t.test('searchParams works with simple strings', t => {
+    let r = searchParams({name: 'evg', email: 'evg1'})
+
+    t.equal(r, 'name=evg&email=evg1')
+    t.end()
+})
+
+t.test('searchParams works with empty keys', t => {
+    let r = searchParams({name: 'evg', email: ''})
+
+    t.equal(r, 'name=evg')
+    t.end()
+})
+
+t.test('searchParams works with empty keys (1)', t => {
+    let r = searchParams({name: '', email: ''})
+
+    t.equal(r, '')
+    t.end()
+})
